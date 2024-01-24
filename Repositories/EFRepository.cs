7b081805e8f2a487ca37f31f606192423b4bfee7
Ryan Blackman <rblackman@mtd.org>
2024-01-24 11:54:23 -0600

    implement disposable

--- Repositories/EFRepository.cs
+++ Repositories/EFRepository.cs
@@ -2,10 +2,11 @@
 
 namespace Mtd.Infrastructure.EFCore.Repositories
 {
-	public abstract class EFRepository<T> where T : class
+	public abstract class EFRepository<T> : IDisposable, IAsyncDisposable where T : class
 	{
 		protected readonly DbContext _dbContext;
 		protected readonly DbSet<T> _dbSet;
+		private bool _disposedValue;
 
 		protected EFRepository(DbContext dbContext)
 		{
@@ -15,5 +16,65 @@ namespace Mtd.Infrastructure.EFCore.Repositories
 		}
 
 		protected IQueryable<T> Query() => _dbSet.AsQueryable();
+
+		#region IDisposable
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!_disposedValue)
+			{
+				if (disposing)
+				{
+					// TODO: dispose managed state (managed objects)
+					_dbContext.Dispose();
+				}
+
+				// TODO: free unmanaged resources (unmanaged objects) and override finalizer
+				// TODO: set large fields to null
+				_disposedValue = true;
+			}
+		}
+
+		// // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+		// ~EFRepository()
+		// {
+		//     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+		//     Dispose(disposing: false);
+		// }
+
+		public void Dispose()
+		{
+			// Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+			Dispose(disposing: true);
+			GC.SuppressFinalize(this);
+		}
+
+		#endregion IDisposable
+
+		#region IAsyncDisposable
+		protected async Task DisposeAsync(bool disposing)
+		{
+			if (!_disposedValue)
+			{
+				if (disposing)
+				{
+					// TODO: dispose managed state (managed objects)
+					await _dbContext.DisposeAsync().ConfigureAwait(false);
+				}
+
+				// TODO: free unmanaged resources (unmanaged objects) and override finalizer
+				// TODO: set large fields to null
+				_disposedValue = true;
+			}
+		}
+
+		public async ValueTask DisposeAsync()
+		{
+			// Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+			await DisposeAsync(disposing: true);
+			GC.SuppressFinalize(this);
+		}
+
+		#endregion IAsyncDisposable
 	}
 }
